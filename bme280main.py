46f98bb1cf5af4766826663e6f588dad9fdc66e1
nathanbent <61810169+nathanbent@users.noreply.github.com>
2022-05-14 19:06:05 -0500

    Updates

--- bme280main.py
+++ bme280main.py
@@ -28,8 +28,6 @@ time_between_reads_0 = 0  # For when this file gets imported
 start_time = time.time()
 built_in_credentials = True
 credentials_file = "credentials.txt"
-i2c = busio.I2C(board.SCL, board.SDA)
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 
 
 def temp_read():  # read temperature, return float with 3 decimal places
